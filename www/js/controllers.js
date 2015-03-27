f52e85c072f1371f66c7d8976f90313feb326d97
Mario Lamacchia <mariolamacchia@gmail.com>
2015-03-27 10:36:47 +0100

    Create js functions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,36 @@
 angular.module('starter.controllers', [])
 
 .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+
     $scope.openMenu = function() {
         $ionicSideMenuDelegate.toggleLeft();
     };
+
+    $scope.load = function() {
+        $scope.comments = angular.fromJson(
+            window.localStorage.getItem('myApp') || []
+        );
+    };
+
+    $scope.save = function() {
+        window.localStorage.setItem('myApp', angular.toJson(
+            $scope.comments || []
+        ));
+    };
+
+    $scope.add = function(comment) {
+        comment.data = new Date();
+        $scope.comments.push(comment);
+        $scope.save();
+        $ionicSideMenuDelegate.toggleLeft();
+        for (var i in comment) delete comment[i];
+    };
+
+    $scope.clear = function() {
+        window.localStorage.removeItem('myApp');
+        $scope.load();
+    };
+
+    $scope.load();
 });
 
